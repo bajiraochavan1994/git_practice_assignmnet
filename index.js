8719e015cec3f452bd101377742316fa51169d99
bajirao chavan <bajiraochavan1994@gmail.com>
2024-02-05 20:18:22 +0530

    new code add

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 // check prime number
-let a = 5;
-for(let i=0;i<a;i++){
-    console.log(8)
+let a = 3;
+let count =0
+for(let i=1;i<=a;i++){
+    if(a%2==0){
+        count++
+    }
 }
+if(count==2){
+    console.log("prime")
+}
+else{console.log("not prime")}
